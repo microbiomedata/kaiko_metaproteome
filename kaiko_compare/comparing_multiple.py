81e4c7497b7ebf011ddd19b59981e8b612955db5
Lee <jayden.lee@pnnl.gov>
2023-07-11 10:01:29 -0700

    added a dictionary with mgf outputs

--- kaiko_compare/comparing_multiple.py
+++ kaiko_compare/comparing_multiple.py
@@ -5,11 +5,14 @@ import os
 mgf_files = glob.glob("C:\\Users\\leej179\\git\\kaiko_metaproteome\\Kaiko_volume\\Kaiko_input_files\\mgf_large_unit_test\\*.mgf")
 kaiko_files = glob.glob("C:\\Users\\leej179\\git\\kaiko_metaproteome_main\\Kaiko_volume\\Kaiko_intermediate\\denovo_output\\mgf_large_unit_test\\*.txt")
 casanovo_files = glob.glob("C:\\Users\\leej179\\git\\casanovo\\output\\*.mztab")
+mgf_dict = {}
 
 def get_mgf_names(mgf_files):
     for mgf_path in mgf_files:
         mgf_name = os.path.basename(mgf_path)
         # print(os.path.splitext(mgf_name)[0])
+        mgf_dict['mgf_names'] = mgf_name
+    print(mgf_dict)
 
 def get_kaiko_names(kaiko_files):
     for kaiko_path in kaiko_files:
@@ -17,16 +20,15 @@ def get_kaiko_names(kaiko_files):
         # print(os.path.splitext(kaiko_name)[0])
 
 def get_casanovo_names(casanovo_files):
-    casanovo_name = ''
     for casanovo_path in casanovo_files:
         tables = mztab.MzTab(casanovo_path)
         casanovo_name_path = tables.metadata['ms_run[1]-location'][8:]
         casanovo_name = os.path.basename(casanovo_name_path)
         # print(os.path.splitext(casanovo_name)[0])
-    return casanovo_name
 
 if __name__ == '__main__':
-    if get_mgf_names(mgf_files) == get_kaiko_names(kaiko_files) == get_casanovo_names(casanovo_files):
-        print('yes')
+    # if get_mgf_names(mgf_files) == get_kaiko_names(kaiko_files) == get_casanovo_names(casanovo_files):
+    #     print('yes')
+    get_mgf_names(mgf_files)
     # get_kaiko_names(kaiko_files)
     # get_casanovo_names(casanovo_files)
